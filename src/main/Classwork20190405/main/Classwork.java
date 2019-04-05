4421be882a1eb9ac40b734ba8fda2da1b60c5dcd
Gumennnik <Gumennnik@rambler.ru>
2019-04-05 21:41:44 +0300

    done some classwork

---
+++ src/main/Classwork20190405/main/Classwork.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.util.Random;
+
+public class Classwork {
+    public static void main(String[] args) {
+        System.out.println("Classwork 05.04.2019");
+
+        System.out.println("Hello World");
+
+        System.out.println("arg[0] is " + args[0]);
+
+        int randomNumberFromInput = Integer.valueOf(args[0]);
+
+        System.out.println("Number from input is " + randomNumberFromInput);
+
+        int randomNumber = new Random().nextInt(200);
+
+        System.out.println("Random number is " + randomNumber);
+
+        if (randomNumber > randomNumberFromInput) {
+            System.out.println("Random generated number greater that number from input");
+        } else {
+            System.out.println("Random generated number less than number from input");
+        }
+
+
+        String moreGrabbedParametrsFromInput = args[1];
+        String andOneMoreFromInput = args[2];
+
+        //"34567890" "Test with space" TestWithoutSpace
+
+        switch (moreGrabbedParametrsFromInput) {
+            case "":
+
+                break;
+            case "Test with space":
+                System.out.println("found \"Test with space\"");
+            case "TestWithoutSpace":
+                System.out.println("Found TestWithoutSpace");
+                break;
+
+            default:
+                System.out.println("Not found");
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+}
