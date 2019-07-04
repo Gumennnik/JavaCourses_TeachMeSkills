e102da71dd5003a66b702245757ecd5e8a5074a5
idiot <gumennnik@rambler.ru>
2019-07-04 23:32:34 +0300

    added some js code

--- scripts/testJS.js
+++ scripts/testJS.js
@@ -1,11 +1,56 @@
 
 $( document ).ready(function() {
     $("#btn").click(
-        
-        function(){
 
-            alert("ALERT!")
+
+
+        function(){
+            
+
+                var searchText = document.getElementById("input").value;
+
+                var queryText = "https://api.themoviedb.org/3/search/movie?api_key=d272326e467344029e68e3c4ff0b4059&language=ru-RU&query=" + searchText;
+
+                $.ajax({
+                    url: queryText
+                }).then(function (data) {
+                    try {
+
+                        alert(data.results[1].json());
+
+
+                    }catch (e) {
+                        window.open("https://stackoverflow.com/search?q=[js]+" + e);
+                    }
+                });
+
+
+/*
+
+* 
+*
+*
+*
+* */
+
+
             return false;
         }
     );
 });
+
+function addRow(firstColumn,secondColumn ) {
+
+    var resultTable = document.getElementById("results");
+    var tableRow = document.createElement("tr");
+    var tableData = document.createElement("td");
+    tableData.innerText = "1";
+    tableRow.appendChild(tableData);
+    tableData = document.createElement("td");
+    tableData.innerText = "2";
+    tableRow.appendChild(tableData);
+    resultTable.appendChild(tableRow);
+
+
+
+}
